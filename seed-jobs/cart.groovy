9ca57aceb38b7e0005db428ecc63782c95957aeb
Balaji A <abj.balaji@gmail.com>
2022-05-15 17:39:15 +0530

    Updating jenkins

--- seed-jobs/cart.groovy
+++ seed-jobs/cart.groovy
@@ -2,9 +2,8 @@ multibranchPipelineJob('example') {
     branchSources {
         git {
             id('123456789') // IMPORTANT: use a constant and unique identifier
-            remote('https://github.com/jenkinsci/job-dsl-plugin.git')
-            credentialsId('github-ci')
-            includes('JENKINS-*')
+            remote('https://github.com/abjbalaji/cart.git')
+           
         }
     }
     orphanedItemStrategy {
